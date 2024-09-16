8009901c58b89e2c69fbafcb8b357ccae1cb2e02
aiimagegen <131513595+RAUNAKtheOP@users.noreply.github.com>
2024-09-16 15:03:56 +0530

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,8 @@
+// JavaScript to handle opening and closing of the sidebar
+function openNav() {
+  document.getElementById("sidebar").style.width = "250px";
+}
+
+function closeNav() {
+  document.getElementById("sidebar").style.width = "0";
+}
